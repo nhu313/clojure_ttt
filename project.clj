a9768ca99ee4af16ff5219b8e1b9adbd3b0cfc77
Nhu Nguyen <nhu313@gmail.com>
2013-08-22 16:51:09 -0500

    updated clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :description "Simple Tic Tac Toe with AI"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
   :plugins [[speclj "2.5.0"]]
   :test-paths ["spec"])
